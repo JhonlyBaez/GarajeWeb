fc349ba614431f9d54db7437e0d7826bdb166b7e
jhonlyB <jhonlybaez@gmail.com>
2018-12-01 13:10:46 -0400

    Agregar archivos de proyecto.

---
+++ GarajeWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GarajeWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
